6651125d237ded2b3021a1b82038a2bd5628efd4
Mahmoud Gabr <mhmudhsham8@gmail.com>
2023-08-23 20:39:44 +0400

    Install MVC and apply admin routes

--- models/product.js
+++ models/product.js
@@ -13,11 +13,15 @@ const getProductsFromFile = cb => {
 };
 
 module.exports = class Product {
-    constructor(title) {
+    constructor(title, imageUrl, description, price) {
         this.title = title;
+        this.imageUrl = imageUrl;
+        this.description = description;
+        this.price = price;
     }
 
     save() {
+        this.id = Math.random().toString();
         getProductsFromFile(products => {
             products.push(this);
             fs.writeFile(p, JSON.stringify(products), err => {
@@ -28,5 +32,12 @@ module.exports = class Product {
 
     static fetchAll(cb) {
         getProductsFromFile(cb);
+    }
+
+    static findById(id, cb) {
+        getProductsFromFile(products => {
+            const product = products.find(p => p.id === id);
+            cb(product);
+        });
     }
 }
